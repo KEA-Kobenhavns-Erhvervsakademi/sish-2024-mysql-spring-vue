edf8daa3ca817575235db02db003354a157d432e
sshine <simon@simonshine.dk>
2024-10-04 00:12:32 +0200

    feat(frontend): Containerize in compose.yaml

--- src/main/java/com/example/restservice/SecurityConfig.java
+++ src/main/java/com/example/restservice/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.restservice;
 
-import java.io.IOException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
